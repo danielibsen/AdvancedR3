01cc1ba3e459e2bdcf5ae7fe346bc08c98618d63
Daniel Ibsen <dbi@ph.au.dk>
2023-11-09 09:41:06 +0100

    making metabolite distributions plot function

--- R/functions.R
+++ R/functions.R
@@ -1,6 +1,6 @@
-#Functions for the project
+# Functions for the project
 
-#Descriptive data analysis------------------------------------------------------
+# Descriptive data analysis------------------------------------------------------
 #' Calculate descriptive statistics of each metabolite.
 #'
 #' @param data cleaned Lipidomics dataset.
@@ -11,8 +11,23 @@ descriptive_stats <- function(cleaned_dataset) {
     descriptive_stats_mean_sd <- cleaned_dataset %>%
         dplyr::group_by(metabolite) %>%
         dplyr::summarise(across(value, list(mean = mean, sd = sd))) %>%
-        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~round(.x, digits = 1)))
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
 
 
     return(descriptive_stats_mean_sd)
+}
+
+# Distribution plot of metabolites-----------------------------------------------
+#' Making a plot of the metabolites
+#'
+#' @param data cleaned Lipidomics dataset.
+#'
+#' @return A plot of the distribution of metabolites
+#'
+plot_metabolite_distribution <- function(cleaned_dataset) {
+    plot <- ggplot2::ggplot(cleaned_dataset, aes(x = value)) +
+        ggplot2::geom_histogram() +
+        ggplot2::facet_wrap(vars(metabolite), scales = "free")
+
+    return(plot)
 }
